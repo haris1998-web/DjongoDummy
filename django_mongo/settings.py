0a5e9577fc793cc5b8e44367a0d2c6373fd7117a
Haris Yousaf <nick@unity-freelancer.de>
2022-09-19 17:51:16 +0500

    Fixes

--- django_mongo/settings.py
+++ django_mongo/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
     'users'
 ]
 
