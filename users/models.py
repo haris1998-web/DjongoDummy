ee8e0e977ed4ba2f0946b77255b1a3f07ea1ffd8
Haris Yousaf <nick@unity-freelancer.de>
2022-09-19 17:47:34 +0500

    Changes

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
